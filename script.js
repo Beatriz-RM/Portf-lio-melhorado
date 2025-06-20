635ff32e927eeecd9bb952f78f630da6aaf97f91
Beatriz Ribeiro Milani <biahrm2008@gmail.com>
2025-06-19 21:03:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("flip-html").addEventListener("click", function () {
+  this.classList.toggle("flipped");
+});
+document.getElementById("flip-js").addEventListener("click", function () {
+  this.classList.toggle("flipped");
+});
+document.getElementById("flip-css").addEventListener("click", function () {
+  this.classList.toggle("flipped");
+});
